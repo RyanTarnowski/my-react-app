7fed9ddf879620267ec74b1a8aedff22dcd375b6
RyanTarnowski <ryan.tarnowski@gmail.com>
2025-03-07 14:17:06 -0700

    Modify app content

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Deploying react app via: VS Code, Git, Github, Netlify </p>
         <a
           className="App-link"
           href="https://reactjs.org"
